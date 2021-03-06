3c1056bef37275346aad2bdb286cc8fbd89e0290
APBorK <1659732@gmail.com>
2021-03-07 01:52:34 +0300

    Update SampleScene.unity

--- Assets/Scripts/SpawnCar.cs
+++ Assets/Scripts/SpawnCar.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class SpawnCar : MonoBehaviour
 {
-    // Скрипт 
+    public Transform spawn;
+    public GameObject[] objToSpawn;
+
+    private float minX;
+    private float minZ;
+    private float maxX;
+    private float maxZ;
+
+    
     void Start()
     {
+        minX = -1.8F + spawn.localScale.x / 2;
+        minX = 9.6F - spawn.localScale.x / 2;
         
+        minZ = spawn.position.z + spawn.localScale.z / 2;
+        minZ = spawn.position.z - spawn.localScale.z / 2;
+        for (int i = 0; i < 5; i++)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(minX,maxX),spawn.position.y,Random.Range(minZ,maxZ));
+            Instantiate(objToSpawn[i], spawnPos, Quaternion.identity);
+        }
+       
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+
     }
 }
